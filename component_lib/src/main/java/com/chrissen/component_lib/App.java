8c1fc75f0d70ad23b871e3a129bcd3a023d3ceba
chrissen <chrissen0814@gmail.com>
2018-12-11 17:57:04 +0800

    init

---
+++ component_lib/src/main/java/com/chrissen/component_lib/App.java
@@ -0,0 +1,16 @@
+package com.chrissen.component_lib;
+
+import android.app.Application;
+
+/**
+ * Function:
+ * <br/>
+ * Describe:
+ * <br/>
+ * Author: chris on 2018/12/11.
+ * <br/>
+ * Email: chrissen0814@gmail.com
+ */
+
+public class App extends Application {
+}
